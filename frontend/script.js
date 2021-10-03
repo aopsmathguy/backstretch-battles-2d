b5b5f33bbfd813291e88a2828ba5f2542eff9ccf
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-10-03 11:55:34 -0400

    Add files via upload

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
-const socket = io('https://salty-chamber-63270.herokuapp.com/', { transports : ['websocket'] });
+// const socket = io('https://salty-chamber-63270.herokuapp.com/', { transports : ['websocket'] });
+const socket = io('http://localhost:3000/', { transports : ['websocket'] });
 
 socket.on('startState', onStartState);
 socket.on('gameState', onGameState);
