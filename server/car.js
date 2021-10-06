a4024625461c3a62506b18fabc37c09db07eba39
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-10-06 00:36:48 -0400

    Update car.js

--- server/car.js
+++ server/car.js
@@ -425,8 +425,8 @@ Car.ParticleWorld = class {
     var p = this.particles[i];
     var addTo = this.getGrid(p.position);
     var success = this.pHashGrid.remove(addTo.x, addTo.y, i);
-    console.log(success);
     delete this.particles[i];
+    return success;
   }
   step(dt){
     for (var i in this.particles){
