3a3b8ecd5ea3211ec19b4db6ab0cd1461f03bdd4
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-10-18 10:27:51 -0400

    Add files via upload

--- frontend/domutil.js
+++ frontend/domutil.js
@@ -1,3 +1,14 @@
 function $e(id){
 	return document.getElementById(id);
 }
+function changeResolution(canvas, scaleFactor) {
+    // Set up CSS size.
+    canvas.style.width = canvas.style.width || canvas.width + 'px';
+    canvas.style.height = canvas.style.height || canvas.height + 'px';
+
+    // Resize canvas and scale future draws.
+    canvas.width = Math.ceil(canvas.width * scaleFactor);
+    canvas.height = Math.ceil(canvas.height * scaleFactor);
+    var ctx = canvas.getContext('2d');
+    ctx.scale(scaleFactor, scaleFactor);
+}
